a184229582ed72d2ed46aac90ae594db0c5ce4c7
Thibaut Tauveron <thibaut.tauveron@gmail.com>
2017-11-09 20:04:31 -0500

    Add lib for Signets API

--- app/src/main/java/com/log515/lambda/wherework/MainActivity.java
+++ app/src/main/java/com/log515/lambda/wherework/MainActivity.java
@@ -3,10 +3,16 @@ package com.log515.lambda.wherework;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.log515.lambda.wherework.model.CoursHoraire;
+import com.log515.lambda.wherework.model.ListeCoursHoraire;
+import com.log515.lambda.wherework.soap.IServiceEvents;
+import com.log515.lambda.wherework.soap.OperationResult;
+import com.log515.lambda.wherework.soap.SignetsMobileSoap;
 import com.log515.lambda.wherework.db.SQLiteHelper;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements IServiceEvents {
 
 
     private SQLiteHelper database;
@@ -16,8 +22,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        database = new SQLiteHelper(this);
 
+        //Test Database
+        database = new SQLiteHelper(this);
         Cursor cursor = database
                 .getReadableDatabase()
                 .rawQuery("SELECT * FROM salles_cours_session WHERE 0", null);
@@ -25,10 +32,29 @@ public class MainActivity extends AppCompatActivity {
         for (String s : cursor.getColumnNames()) {
             System.out.println(s);
         }
-
-
         cursor.close();
 
+
+        //Test Signets API
+        SignetsMobileSoap signetsMobileSoap = new SignetsMobileSoap(this);
+
+        signetsMobileSoap.lireHoraireAsync("A2017", "LOG");
+
+    }
+    @Override
+    public void Starting() {
+        Log.i("SignetsWebServices", "START : SOAP request starting");
+    }
+
+    @Override
+    public void Completed(OperationResult result) {
+        if(result.Result instanceof ListeCoursHoraire) {
+            ListeCoursHoraire liste = (ListeCoursHoraire)result.Result;
+            for (CoursHoraire listeCour : liste.listeCours) {
+                System.out.println(listeCour.sigle);
+            }
+
+        }
 
     }
 }
