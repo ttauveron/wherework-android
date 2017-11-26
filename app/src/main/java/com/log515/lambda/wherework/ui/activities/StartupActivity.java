e235ca52f03808bfdddbfeaacb9d7a1fc1f47046
Thibaut Tauveron <thibaut.tauveron@gmail.com>
2017-11-26 01:38:02 -0500

    Add internet activation popup on first login if not connected

--- app/src/main/java/com/log515/lambda/wherework/ui/activities/StartupActivity.java
+++ app/src/main/java/com/log515/lambda/wherework/ui/activities/StartupActivity.java
@@ -1,8 +1,14 @@
 package com.log515.lambda.wherework.ui.activities;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -39,22 +45,25 @@ public class StartupActivity extends AppCompatActivity {
         SharedPreferences sharedPref = getPreferences(MODE_PRIVATE);
         boolean alarmScheduled = sharedPref.getBoolean(ALARM_SCHEDULED_PREF_KEY, false);
 
+
         if (!alarmScheduled) {
-            progressBarLoading.setVisibility(View.VISIBLE);
-            database.syncDB()
-                    .subscribeOn(Schedulers.newThread())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(o -> {
-                        WakefulIntentService.scheduleAlarms(new WeeklyListener(), getApplicationContext(), false);
+            if (isConnected(this)) {
+                progressBarLoading.setVisibility(View.VISIBLE);
+                database.syncDB()
+                        .subscribeOn(Schedulers.newThread())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(o -> {
+                            WakefulIntentService.scheduleAlarms(new WeeklyListener(), getApplicationContext(), false);
 
-                        SharedPreferences.Editor editor = sharedPref.edit();
-                        editor.putBoolean(ALARM_SCHEDULED_PREF_KEY, true);
-                        editor.apply();
+                            SharedPreferences.Editor editor = sharedPref.edit();
+                            editor.putBoolean(ALARM_SCHEDULED_PREF_KEY, true);
+                            editor.apply();
 
-                        progressBarLoading.setVisibility(View.GONE);
+                            progressBarLoading.setVisibility(View.GONE);
 
-                        launchActivity();
-                    });
+                            launchActivity();
+                        });
+            }
         } else {
             launchActivity();
         }
@@ -63,5 +72,37 @@ public class StartupActivity extends AppCompatActivity {
     private void launchActivity() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    public boolean isConnected(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo wifiInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        NetworkInfo mobileInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+
+        if ((wifiInfo != null && wifiInfo.isConnected()) || (mobileInfo != null && mobileInfo.isConnected())) {
+            return true;
+        } else {
+            showDialog();
+            return false;
+        }
+    }
+
+    private void showDialog() {
+        new AlertDialog.Builder(this)
+                .setCancelable(false)
+                .setMessage(getString(R.string.wifi_required))
+                .setPositiveButton(getString(R.string.connect), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+                    }
+                })
+                .setNegativeButton(getString(R.string.quit), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        StartupActivity.this.finish();
+                    }
+                })
+                .show();
+
+
     }
 }
